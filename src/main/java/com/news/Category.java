706d6ffb7568b0faa0790eb6686ff1ca3d7f40a3
rishikumars <rishikumar.siva@gmail.com>
2018-07-02 14:30:21 +0530

    new commit

---
+++ src/main/java/com/news/Category.java
@@ -0,0 +1,12 @@
+package com.news;
+
+public class Category {
+    public static String BUSINESS="business";
+    public static String ENTERTAINMENT ="entertainment";
+    public static String GENERAL="general";
+    public static String HEALTH="health";
+    public static String SCIENCE="science";
+    public static String SPORTS="sports";
+    public static String TECHNOLOGY="technology";
+
+}
